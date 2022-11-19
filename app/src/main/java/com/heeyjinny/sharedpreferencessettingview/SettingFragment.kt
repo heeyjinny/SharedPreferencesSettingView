be91b631ae736c0ce1c2b0512fe798c4d418cc4d
heeyjinny <heeyjinny@gmail.com>
2022-11-19 11:13:31 +0900

    구조를 정의한 뷰를 프래그먼트 클래스를 생성하여 연결

---
+++ app/src/main/java/com/heeyjinny/sharedpreferencessettingview/SettingFragment.kt
@@ -0,0 +1,18 @@
+package com.heeyjinny.sharedpreferencessettingview
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+//PreferenceFragmentCompat 추상클래스를 상속받음
+class SettingFragment: PreferenceFragmentCompat() {
+
+    //onCreatePreferences() 메서드 오버라이드
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
+        //addpreferencesFromResource 호출하고
+        //PreferenceScreen이 정의된 preference.xml 파일의 경로를 파라미터로 전달하면
+        //설정 항목의 뷰가 자동으로 생성됨
+        addPreferencesFromResource(R.xml.preferences)
+
+    }
+}
