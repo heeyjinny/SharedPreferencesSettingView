33013f251fd6f469c2465a3378a0df526e55a760
heeyjinny <heeyjinny@gmail.com>
2022-11-20 21:27:27 +0900

    설정값 사용하기

--- app/src/main/java/com/heeyjinny/sharedpreferencessettingview/MainActivity.kt
+++ app/src/main/java/com/heeyjinny/sharedpreferencessettingview/MainActivity.kt
@@ -2,6 +2,8 @@ package com.heeyjinny.sharedpreferencessettingview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.preference.PreferenceManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +31,28 @@ class MainActivity : AppCompatActivity() {
         //Source set : main (위 타입 지정하면 자동으로...)
         //Directory name : xml (위 타입 지정하면 자동으로...)
 
-        //생성한 preferences.xml 수정
+        //생성한 preferences.xml 수정성
+
+
+        //FIN
+        //설정값 사용
+        //XML 레이아웃을 조금 추가하면 쉽게 Preference 화면을 만들 수 있음
+
+        //각각의 Preference를 설정하면 자동으로 옵션이 저장되고 저장된 옵션을 읽으려면 key 이용
+        //PreferenceManager에서 getDefaultSharedPreferences로 SharedPreference 가져와 사용
+
+        //저장된 데이터가 boolean인 경우 getBoolean(key, default)
+        //초기값이 없는 경우 default로 설정한 것을 가져오게 됨
+        //문자열의 경우 getString을 사용합니다. Int와 Long, Float도 모두 동일한 방식 사용
+
+        //PreferenceManager를 통해 SharedPreference 가져와 shared에 저장
+        val shared = PreferenceManager.getDefaultSharedPreferences(this)
+
+        //키 값과 기본값 지정하여 현재 설정된 데이터 가져오기
+        val checkboxValue = shared.getBoolean("key_add_shortcut",false)
+        val switchValue = shared.getBoolean("key_switch_on", false)
+        val name = shared.getString("key_edit_nam", "이름을 입력하세요")
+        val selected = shared.getString("key_set_item", "목록을 선택하세요")
 
     }
 }
